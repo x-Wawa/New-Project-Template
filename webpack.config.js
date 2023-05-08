19dd6e67b070081768e2e8a33846e91db1c6a969
x-Wawa <wayan.neel.amara@gmail.com>
2023-05-08 22:51:40 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.tsx",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js",
+    },
+
+    target: "web",
+    devServer: {
+        port: "3000",
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".css"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
